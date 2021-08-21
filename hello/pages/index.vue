88fd6ebce2fe7540528d0eed146c199a6540fbda
hot-and-cool <takeuchi@eda-inc.jp>
2021-08-21 13:08:52 +0900

    first commit

---
+++ hello/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <p>{{ message }}</p>
+    <hr>
+    <router-link to="/price">Price Page</router-link>
+  </section>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello word'
+    }
+  }
+}
+</script>
---
+++ nuxt-async/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }}, {{ user.name }}, {{ user.company.name }}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/usersxxx'
+
+export default {
+  asyncData({ params }) {
+    return axios.get(url)
+      .then((res) => {
+        return { users: res.data }
+      })
+      .catch((e) => {
+        // console.log(e.response.status)
+        error({ statusCode: e.response.status, message: e.message })
+      })
+  }
+}
+</script>
